48d74fe9ab03466b39cb234c13a178639a4ee4da
Myroslav <myroslavtp@gmail.com>
2022-10-03 12:19:25 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -28,27 +28,66 @@ $('.slider').slick({
 
 $(document).ready(function(){
   $('.guitars').slick({
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 400,
-    slidesToShow: 1,
+    slidesToShow: 5,
     adaptiveHeight: true,
-    arrows: false,
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 4000,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='./img/prev.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='./img/next.png'>",
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+        slidesToShow: 3,
+        centerMode: false, 
+        dots: true,
+        arrows: false,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 1,
+        centerMode: false, 
+        dots: true,
+        arrows: false,
+        }
+      }
+     ]
   });
 
 /// SLIDER Companies
 
 $('.company').slick({
-  dots: true,
+  dots: false,
   infinite: true,
-  speed: 300,
-  slidesToShow: 1,
+  speed: 400,
+  slidesToShow: 6,
   adaptiveHeight: true,
-  arrows: false,
-  centerMode: true,
-  autoplay: true
+  arrows: true,
+  centerMode: false,
+  autoplay: true,
+  prevArrow:"<img class='a-left control-c prev slick-prev' src='./img/prev.png'>",
+  nextArrow:"<img class='a-right control-c next slick-next' src='./img/next.png'>",
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+      slidesToShow: 1,
+      speed: 400,
+      centerMode: false, 
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+      adaptiveHeight: true,
+      autoplay: true,
+      }
+    }
+   ]
 });
 
 
